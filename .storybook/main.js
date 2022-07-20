55a3a5e9be5e61c0073bbcf4d2e616765faf8415
iyha-1999 <iyha1999end@gmail.com>
2022-07-20 23:27:24 +0900

    StorybookとMUIのemotionバージョンの違いの問題の吸収

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  webpackFinal(config) {
+    delete config.resolve.alias["emotion-theming"];
+    delete config.resolve.alias["@emotion/styled"];
+    delete config.resolve.alias["@emotion/core"];
+    return config;
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
